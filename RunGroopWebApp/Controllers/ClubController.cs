407eff62470aacb2ec1e57fb756424a12df48838
qvixnh <nguyenvinh22012002@gmail.com>
2023-10-20 10:26:00 +0700

    Creating Views for Clubs and Races

---
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+        public IActionResult Index() //C
+        {
+
+            var clubs = _context.Clubs.ToList();//M
+            return View(clubs);//V
+        }
+    }
+}
---
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
