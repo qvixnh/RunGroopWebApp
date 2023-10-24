56b5031434d6bf8fe69051cd52b85e191c529f57
qvixnh <nguyenvinh22012002@gmail.com>
2023-10-24 19:49:49 +0700

    Login page

---
+++ RunGroopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly SignInManager<AppUser> _signInManager;
+
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM  )
+        {
+            if(!ModelState.IsValid)
+            {
+
+                return View(loginVM);
+            }
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if(user != null) {
+                //user is found, check password
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    //password cerrect, sign in
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race"); 
+                    }
+                }
+                //password is incorrect
+                TempData["Error"] = "Wrong credential. Please, try again";
+                return View(loginVM);
+            }
+            //user not found
+            TempData["Error"] = "Wrong credential. Please try again";
+            return View(loginVM);
+        }
+    }
+}
