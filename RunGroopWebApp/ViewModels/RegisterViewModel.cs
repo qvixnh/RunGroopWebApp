c0dad1b2b99d02e19882b45414f68b9fbc43488d
qvixnh <nguyenvinh22012002@gmail.com>
2023-10-25 19:36:44 +0700

    Register Page

---
+++ RunGroopWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Confirm password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage ="Password do not match")]
+        [DataType(DataType.Password)]
+
+        public string ConfirmPassword { get; set; }
+
+    }
+}
