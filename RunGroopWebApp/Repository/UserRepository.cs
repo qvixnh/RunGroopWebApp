b9ef441dc94a5496422ef11cdf6706cc44ad1f1b
qvixnh <nguyenvinh22012002@gmail.com>
2023-10-27 21:45:00 +0700

    List/Detail Users

---
+++ RunGroopWebApp/Repository/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using System.Runtime.InteropServices;
+
+namespace RunGroopWebApp.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public bool Add(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<AppUser>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            return await _context.AppUsers.FindAsync(id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(AppUser user)
+        {
+            _context.Update(user);
+            return Save();
+        }
+    }
+}
